b24ddadcf1da99b2794a6a0883d2f0f6e703fef2
Bruce Becker <brucellino@gmail.com>
2015-05-29 17:06:21 +0200

    Update default.py
    Changed the name of the discourse host

--- src/default.py
+++ src/default.py
@@ -1,5 +1,5 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    DISCUOURSE_URL = 'http://discuss.example.com'
+    DISCUOURSE_URL = 'http://discourse.sci-gaia.eu'
     SECRET_KEY = 'd836444a9e4084d5b224a60c208dce14'
