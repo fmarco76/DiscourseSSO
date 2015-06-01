ce7ac597cf3ce9f18f0b6e0bd9cf4dce60c6c6f5
Marco Fargetta <marco.fargetta@ct.infn.it>
2015-06-01 17:00:43 +0200

    Increase coverage
    Missed only the run as python application

--- tests/test_sso.py
+++ tests/test_sso.py
@@ -113,3 +113,26 @@ class Test_sso():
                                      'hbF9pZD1oZWxsbzEyMw%3D%3D%0A&sig=b8'
                                      '2864ea726a3fced213acf19c62296f79a64'
                                      'cd7a04921c951dd7d05e2c0165e')
+
+    def test_authentication_generation_with_full_name(self):
+        """Test the authentication are properly send to Discourse"""
+        with app.test_request_context('/sso/auth',
+                                      method='GET',
+                                      environ_base={
+                                          'givenName': 'sam',
+                                          'sn': 'big',
+                                          'mail': 'test@test.com',
+                                          'eppn': 'hello123'}
+                                      ) as req:
+            req.session['nonce'] = 'nonce=cb68251eefb5211e58c00ff1395f0c0b'
+            resp = sso.user_authz()
+            assert resp.status_code == 302
+            assert resp.location == ('http://discuss.example.com/session/'
+                                     'sso_login?sso=bm9uY2U9Y2I2ODI1MWVlZ'
+                                     'mI1MjExZTU4YzAwZmYxMzk1ZjBjMGImbmFt'
+                                     'ZT1zYW0gYmlnJnVzZXJu%0AYW1lPXNhbWJp'
+                                     'Z19iNjQyJmVtYWlsPXRlc3QlNDB0ZXN0LmN'
+                                     'vbSZleHRlcm5hbF9pZD1oZWxsbzEy%0AMw%'
+                                     '3D%3D%0A&sig=12620fa583a8d03f17eb7b'
+                                     'dd52ef1e2ae25641b14b164e5621aea3762'
+                                     '0e00f06')
